973002418942892000daea5bd0f5e064721c86a4
keeks5456 <Akikogreen08@gmail.com>
2020-06-11 10:46:00 -0700

    done?

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,41 @@
 class Author
-  attr_accessor :name
+  attr_accessor :name, :article
 
-
+    @@all = []
   def initialize(name)
     @name = name
-  
+    # @article = article
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def article
+    # binding.pry
+    Article.all.select {|author| author.author == self} 
+  end
+
+  def magazine #come back to
+    # binding.pry
+    article.map {|mags| mags.magazine}.uniq
+  end
+
+  def add_article(magazing, title)
+    new_article = Article.new(self, magazine, title)
+  end
+    
+  # def topic_areas
+  #   binding.pry
+  #   Article.all.select do |contribute|
+  #     contribute.magazine == self
+  #   end.uniq
+  # end
+  # I want to iterate over the Article class
+  #find all the categories from the magazine instance 
+  # and find the author that it is associated with
+
+  
 
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,52 @@
 class Magazine
-  attr_accessor :name, :category
+  attr_accessor :name, :category, :article
 
-
+@@all = []
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
 
+
+  def self.all 
+    @@all
+  end
+
+  def contributors
+    Article.all.select do |contribute|
+      contribute.author == self
+    end
+  end
+
+  def self.find_by_name(name)
+    binding.pry
+    all.find do |mags| 
+      mags.name == name
+  end
+end
+
+def article_titles
+  binding.pry
+  Article.all.find_all do |article|
+  article.title == self
+  
+end
+  # end
+end
+
+# talks to the Articles.all and iterates through it
+def contributing_authors
+  articles = 1
+  Article.all.find_all do |article|
+    # if article.author > articles
+      # article.author
+  end
+# binding.pry
+end
+#  talks to the Articles class
+# needs to find all of the authors that have written more than one article
+#using article = 1, i want to compare if the authors found with two magazines
+# if it returns true then return that author with their magazines
 
 end
